844eb9fefcf0299eb0932e14f2f70aec523002a2
matos-nelson <nmatos6@gatech.edu>
2023-07-03 13:03:38 -0500

    Added integration tests for updating maintenance request.

--- src/main/java/org/rent/circle/maintenance/api/resource/MaintenanceResource.java
+++ src/main/java/org/rent/circle/maintenance/api/resource/MaintenanceResource.java
@@ -2,18 +2,23 @@ package org.rent.circle.maintenance.api.resource;
 
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.rent.circle.maintenance.api.dto.maintenance.MaintenanceRequestDto;
 import org.rent.circle.maintenance.api.dto.maintenance.SaveMaintenanceRequestDto;
+import org.rent.circle.maintenance.api.dto.maintenance.UpdateMaintenanceRequestDto;
 import org.rent.circle.maintenance.api.service.MaintenanceService;
 
 @AllArgsConstructor
 @Path("/maintenance")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Slf4j
 public class MaintenanceResource {
 
     private final MaintenanceService maintenanceService;
@@ -22,5 +27,12 @@ public class MaintenanceResource {
     @Path("/request")
     public Long saveMaintenanceRequest(@Valid SaveMaintenanceRequestDto saveMaintenanceRequestDto) {
         return maintenanceService.saveRequest(saveMaintenanceRequestDto);
+    }
+
+    @PATCH
+    @Path("/request")
+    public MaintenanceRequestDto updateMaintenanceRequest(
+        @Valid UpdateMaintenanceRequestDto updateMaintenanceRequestDto) {
+        return maintenanceService.updateRequest(updateMaintenanceRequestDto);
     }
 }
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -9,3 +9,6 @@ INSERT INTO category(id, name) values (8, 'Landscape');
 INSERT INTO category(id, name) values (9, 'Pest Control');
 INSERT INTO category(id, name) values (10, 'Plumbing/Leaks');
 INSERT INTO category(id, name) values (11, 'Windows');
+
+INSERT INTO maintenance_request(id, owner_id, resident_id, property_id, category_id, description, status) values (100, 1, 1, 1, 1, 'Windows', 'IN_PROGRESS');
+INSERT INTO maintenance_request(id, owner_id, resident_id, property_id, category_id, description, status) values (200, 2, 2, 2, 2, 'Windows', 'COMPLETED');
