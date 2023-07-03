ac919715057ac8010859103376a5d1033841eed1
matos-nelson <nmatos6@gatech.edu>
2023-07-03 12:38:45 -0500

    Implemented update maintenance request into service.

--- src/main/java/org/rent/circle/maintenance/api/dto/maintenance/MaintenanceRequestDto.java
+++ src/main/java/org/rent/circle/maintenance/api/dto/maintenance/MaintenanceRequestDto.java
@@ -1,24 +1,18 @@
 package org.rent.circle.maintenance.api.dto.maintenance;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.rent.circle.maintenance.api.dto.category.CategoryDto;
 
-@Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class MaintenanceRequestDto {
 
-    private Long id;
     private Long ownerId;
     private Long residentId;
     private Long propertyId;
-    private Long categoryId;
+    private CategoryDto category;
     private String description;
     private String status;
     private LocalDateTime completedAt;
