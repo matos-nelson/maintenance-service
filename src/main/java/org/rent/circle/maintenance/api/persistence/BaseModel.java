43fdd05297405be3b2dbf026d5f3983a9555b4ba
matos-nelson <nmatos6@gatech.edu>
2023-07-08 20:03:37 -0500

    Exposed id and updatedAt fieds for maintenace request dto.

--- src/main/java/org/rent/circle/maintenance/api/persistence/BaseModel.java
+++ src/main/java/org/rent/circle/maintenance/api/persistence/BaseModel.java
@@ -3,10 +3,14 @@ package org.rent.circle.maintenance.api.persistence;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @MappedSuperclass
+@Setter
+@Getter
 public abstract class BaseModel {
 
     @Column(name = "created_at")
--- src/main/java/org/rent/circle/maintenance/api/service/mapper/MaintenanceMapper.java
+++ src/main/java/org/rent/circle/maintenance/api/service/mapper/MaintenanceMapper.java
@@ -2,6 +2,7 @@ package org.rent.circle.maintenance.api.service.mapper;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.rent.circle.maintenance.api.dto.maintenance.MaintenanceRequestDto;
 import org.rent.circle.maintenance.api.dto.maintenance.SaveMaintenanceRequestDto;
 import org.rent.circle.maintenance.api.persistence.model.MaintenanceRequest;
@@ -11,6 +12,8 @@ public interface MaintenanceMapper {
 
     MaintenanceRequest toModel(SaveMaintenanceRequestDto saveMaintenanceRequestDto);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "updatedAt", source = "updatedAt")
     MaintenanceRequestDto toDto(MaintenanceRequest maintenanceRequest);
 
     List<MaintenanceRequestDto> toDtoList(List<MaintenanceRequest> maintenanceRequests);
