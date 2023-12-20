ac0d954760ae9e1093200c906ce2fa9f82781866
matos-nelson <nmatos6@gatech.edu>
2023-12-19 19:27:01 -0600

    Added Labor entity.

--- src/main/java/org/rent/circle/maintenance/api/persistence/model/MaintenanceRequest.java
+++ src/main/java/org/rent/circle/maintenance/api/persistence/model/MaintenanceRequest.java
@@ -1,5 +1,6 @@
 package org.rent.circle.maintenance.api.persistence.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -7,9 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -53,4 +56,8 @@ public class MaintenanceRequest extends BaseModel {
 
     @Column(name = "completed_at")
     private LocalDateTime completedAt;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "maintenance_request_id", referencedColumnName = "id", nullable = false)
+    private List<Labor> labors;
 }
---
+++ src/test/java/org/rent/circle/maintenance/api/persistence/model/LaborTest.java
@@ -0,0 +1,41 @@
+package org.rent.circle.maintenance.api.persistence.model;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.quarkus.test.junit.QuarkusTest;
+import java.time.LocalDateTime;
+import org.force66.beantester.BeanTester;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class LaborTest {
+
+    @Test
+    public void Labor_SettersAndGetters_ShouldWork() {
+        // Arrange
+        BeanTester beanTester = new BeanTester();
+        beanTester.addExcludedField("workDate");
+        beanTester.addExcludedField("createdAt");
+        beanTester.addExcludedField("updatedAt");
+
+        // Act
+        beanTester.testBean(Labor.class);
+
+        // Assert
+
+        // Test Excluded fields
+
+        // Arrange
+        Labor labor = new Labor();
+
+        // Act
+        labor.setWorkDate(LocalDateTime.now());
+        labor.setUpdatedAt(LocalDateTime.now());
+        labor.setCreatedAt(LocalDateTime.now());
+
+        // Assert
+        assertNotNull(labor.getWorkDate());
+        assertNotNull(labor.getCreatedAt());
+        assertNotNull(labor.getUpdatedAt());
+    }
+}
